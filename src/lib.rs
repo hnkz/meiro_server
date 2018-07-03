8e5690574458d9470eeb19a9d65d2efaa3134150
Kazuki Hanai <cs16075@s.inf.shizuoka.ac.jp>
2018-07-03 15:33:40 +0900

    init repo
    first commit

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+extern crate websocket;
+
+pub mod ws_server;
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![feature(plugin, decl_macro)]
+#![plugin(rocket_codegen)]
+
+#[macro_use]
+extern crate rocket;
+extern crate meiro_server;
+
+use meiro_server::ws_server;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+fn main() {
+    ws_server::init_ws_server();
+    rocket::ignite().mount("/", routes![hello]).launch();
+}
