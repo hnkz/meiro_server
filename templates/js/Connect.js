fae377966a64e4ff4d4a5862f2457b5cff48216a
Kazuki Hanai <cs16075@s.inf.shizuoka.ac.jp>
2018-07-23 16:45:23 +0900

    fixfix
    fixfix

--- templates/js/Connect.js
+++ templates/js/Connect.js
@@ -1,7 +1,12 @@
-var connection = new WebSocket("ws://10.75.140.109:2794", "rust-websocket");
+var connection = new WebSocket("ws://192.168.0.87:2794", "rust-websocket");
 
 // receive positions
-var playerPos = [[0, 4, 0], [0, 4, 0], [0, 4, 0], [0, 4, 0]];
+var playerPos = [
+    [0, 4, 0],
+    [0, 4, 0],
+    [0, 4, 0],
+    [0, 4, 0]
+];
 var mapPos = new Array();
 var itemType = new Array();
 var itemPos = new Array();
@@ -20,83 +25,83 @@ connection.onerror = onError;
 connection.onmessage = onMessage;
 
 function onOpen() {
-  console.log("connection success");
+    console.log("connection success");
 }
 
 function onClose(event) {
-  console.log(event);
+    console.log(event);
 }
 
 function onError(error) {
-  console.log(error);
+    console.log(error);
 }
 
 function onMessage(message) {
-  let data = JSON.parse(message.data);
-  //console.log(data);
+    let data = JSON.parse(message.data);
+    //console.log(data);
 
-  // game state
-  if (gameState == 0) {
-    gameState++;
-  }
-
-  // id
-  if (data.id !== undefined && !getPlayerID) {
-    playerID = data.id;
-    getPlayerID = true;
-  }
-
-  // map
-  if (data.map) {
-    for (i = 0; i < data.map.length; i++) {
-      mapPos[i] = data.map[i];
+    // game state
+    if (gameState == 0) {
+        gameState++;
     }
-  }
 
-  // player
-  if (data.player) {
-    for (i = 0; i < data.player.length; i++) {
-      playerPos[i][0] = data.player[i].x;
-      playerPos[i][1] = data.player[i].y;
-      playerPos[i][2] = data.player[i].z;
+    // id
+    if (data.id !== undefined && !getPlayerID) {
+        playerID = data.id;
+        getPlayerID = true;
     }
-  }
 
-  // item
-  if (data.item) {
-    for (i = 0; i < data.item.length; i++) {
-      itemType[i] = data.item[i].type;
-      itemPos[i] = data.item[i].pos;
+    // map
+    if (data.map) {
+        for (i = 0; i < data.map.length; i++) {
+            mapPos[i] = data.map[i];
+        }
     }
-  }
 
-  // get
-  getItemID = -1;
-  if (data.get) {
-    getItemID = data.get;
-  }
+    // player
+    if (data.player) {
+        for (i = 0; i < data.player.length; i++) {
+            playerPos[i][0] = data.player[i].x;
+            playerPos[i][1] = data.player[i].y;
+            playerPos[i][2] = data.player[i].z;
+        }
+    }
 
-  // chat
-  if (data.chat) {
-    let content = document.getElementById("text");
-    let newContent = "Player" + data.chat.id + ": " + data.chat.content + "<br>";
-    content.innerHTML = newContent + content.innerHTML;
-    document.getElementById("chat").value = "";
-  }
+    // item
+    if (data.item) {
+        for (i = 0; i < data.item.length; i++) {
+            itemType[i] = data.item[i].type;
+            itemPos[i] = data.item[i].pos;
+        }
+    }
+
+    // get
+    getItemID = -1;
+    if (data.get) {
+        getItemID = data.get;
+    }
+
+    // chat
+    if (data.chat) {
+        let content = document.getElementById("text");
+        let newContent = "Player" + data.chat.id + ": " + data.chat.content + "<br>";
+        content.innerHTML = newContent + content.innerHTML;
+        document.getElementById("chat").value = "";
+    }
 }
 
 function sendPos(pos) {
-  let message = `{ "pos": [${pos.x}, ${pos.y}, ${pos.z}] }`;
-  connection.send(message);
+    let message = `{ "pos": [${pos.x}, ${pos.y}, ${pos.z}] }`;
+    connection.send(message);
 }
 
 function sendGet(id) {
-  let message = `{ "get": ${id} }`;
-  connection.send(message);
+    let message = `{ "get": ${id} }`;
+    connection.send(message);
 }
 
 function sendChat() {
-  let content = document.getElementById("chat").value;
-  let message = `{ "chat": { "id": ${playerID}, "content": "${content}" } }`;
-  connection.send(message);
+    let content = document.getElementById("chat").value;
+    let message = `{ "chat": { "id": ${playerID}, "content": "${content}" } }`;
+    connection.send(message);
 }
